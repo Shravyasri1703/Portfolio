7c773270aaffc791b8b40eb4802438a9c99516da
Shravyasri1703 <shravya.kmp@gmail.com>
2024-04-16 17:41:28 +0530

    new change

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ function Navbar() {
     <div className='wrapper'>
    <motion.span initial={{opacity:0, scale:0.5}} 
    animate={{opacity:1, scale:1}} 
-   transition={{duration:0.5}} >My Profile</motion.span>
+   transition={{duration:0.5}} >Welcome To My Profile</motion.span>
    <div className='social'>
     <a href='#'><img src='/facebook.png' alt='' /> </a>
     <a href='#'><img src='/instagram.png' alt='' /></a>
