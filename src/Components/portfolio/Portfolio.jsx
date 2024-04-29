22f740a7c2290b8bd72996eb28f096953be30afd
Shravyasri1703 <shravya.kmp@gmail.com>
2024-04-29 19:59:52 +0530

    images of projects added

--- src/Components/portfolio/Portfolio.jsx
+++ src/Components/portfolio/Portfolio.jsx
@@ -9,7 +9,7 @@ const items = [
     {
         id:1,
         title: 'AI Article Summarizer',
-        img:'https://images.pexels.com/photos/18392123/pexels-photo-18392123/free-photo-of-heart-carved-in-rock.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load',
+        img:'./Ai_Article.png',
         desc: 'A modern and user-friendly interface, offering an intuitive experience for users. Users can input the URL of a lengthy article, and the web app utilizes AI to provide a concise summary of the article content.The app includes a history feature, allowing users to save summaries locally, providing a convenient way to revisit and manage their reading history. Enables users to easily share or store the summarized content by copying it to their clipboard. Utilizes the advanced capabilities of Redux Toolkit (RTK) Query for making API requests. These requests fire conditionally based on specific criteria, optimizing data fetching and management and many more, including code architecture and reusability',
         link: 'https://github.com/Shravyasri1703/AI_SUMMARIZER',
     },
@@ -17,21 +17,21 @@ const items = [
     {
         id:2,
         title: 'Amazon Clone',
-        img:'https://images.pexels.com/photos/17151647/pexels-photo-17151647/free-photo-of-several-transparent-compact-audio-cassettes-with-visible-tape-and-red-inner-reels-placed-randomly-on-a-blue-background-selective-focus-technique.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load',
+        img:'./Ama_Clone.png',
         desc: 'Experience the convenience of online shopping with our Amazon clone project built using React. Sign up and log in securely with your email and password, browse through a wide range of products, add items to your cart, and seamlessly checkout using Stripe for secure payments. Enjoy a user-friendly interface and streamline your shopping experience with our reliable Firebase backend integration.',
        link : 'https://github.com/Shravyasri1703/Amazon_Clone'
     },
     {
         id:3,
         title: 'Code Hub',
-        img:'https://images.pexels.com/photos/17151647/pexels-photo-17151647/free-photo-of-several-transparent-compact-audio-cassettes-with-visible-tape-and-red-inner-reels-placed-randomly-on-a-blue-background-selective-focus-technique.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load',
+        img:'./codehub.png',
         desc: 'Embark on a journey through the code cosmos with our GitHub Clone project, crafted with React and fortified by the MERN stack along with the stylish touch of TailwindCSS. Dive into the world of authentication and authorization with Passport.js, seamlessly integrating GitHub authentication for a secure login experience. Explore the vast expanse of GitHub user profiles and repositories, harnessing the power of the GitHub API. With lightning-fast filtering capabilities on the client side, discover, organize, and navigate through repositories with unparalleled efficiency',
         link : 'https://github.com/Shravyasri1703/Code_Hub'
     },
     {
         id:4,
         title: 'Chatify',
-        img:'https://images.pexels.com/photos/17151647/pexels-photo-17151647/free-photo-of-several-transparent-compact-audio-cassettes-with-visible-tape-and-red-inner-reels-placed-randomly-on-a-blue-background-selective-focus-technique.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load',
+        img:'./Chat.png',
         desc: 'Discover seamless communication with our React-based chatting app. Sign up and log in securely using email and password authentication. Connect with friends in real-time, share photos, and engage in lively conversations. Easily find and chat with friends online, while having the flexibility to block unwanted contacts. Powered by Firebase, enjoy a reliable backend infrastructure ensuring smooth user experience and data security.',
         link : 'https://github.com/Shravyasri1703/Full_Stack_Chat'
     }
@@ -47,7 +47,7 @@ const Single = ({item}) => {
         <div className="container">
             <div className="wrapper">
               <div className="imageContainer" ref={ref}>
-              <img className='pic' src='./mountains.png' alt='' />
+              <img className='pic' src={item.img} alt='' />
                </div>  
            
             <motion.div className="textContainer" style={{ y }}>
